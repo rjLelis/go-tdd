59be6a84a8ebeb0cd98a78bd6adf63637fac235e
rjLelis <renatojlelis@gmail.com>
2020-04-22 10:58:19 -0300

    Refactor Ola function with tdd

--- ola/ola_test.go
+++ ola/ola_test.go
@@ -3,10 +3,25 @@ package main
 import "testing"
 
 func TestOla(t *testing.T) {
-	resultado := Ola("Renato")
-	esperado := "Olá, Renato"
 
-	if resultado != esperado {
-		t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
+	verificarMensagemCorrenta := func(t *testing.T, resultado, esperado string) {
+		t.Helper()
+		if resultado != esperado {
+			t.Errorf("resultado %s, esperado %s", resultado, esperado)
+		}
 	}
+
+	t.Run("diz olá para as pessoas", func(t *testing.T) {
+		resultado := Ola("Renato")
+		esperado := "Olá, Renato"
+
+		verificarMensagemCorrenta(t, resultado, esperado)
+	})
+
+	t.Run("diz Olá, mundo quando uma string vazia for passada", func(t *testing.T) {
+		resultado := Ola("")
+		esperado := "Olá, mundo"
+
+		verificarMensagemCorrenta(t, resultado, esperado)
+	})
 }
