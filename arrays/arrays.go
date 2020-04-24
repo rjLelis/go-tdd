a96fd766df3332683949c36bddb2c889d7fde499
rjLelis <renatojlelis@gmail.com>
2020-04-24 11:14:46 -0300

    Finish arrays chapter

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -7,3 +7,23 @@ func Soma(numeros []int) (soma int) {
 
 	return
 }
+
+func SomaTudo(numerosParaSomar ...[]int) (somas []int) {
+	for _, numeros := range numerosParaSomar {
+		somas = append(somas, Soma(numeros))
+	}
+
+	return
+}
+
+func SomaTodoOResto(numerosParaSomar ...[]int) (somas []int) {
+	for _, numeros := range numerosParaSomar {
+		if len(numeros) == 0 {
+			somas = append(somas, 0)
+		} else {
+			final := numeros[1:]
+			somas = append(somas, Soma(final))
+		}
+	}
+	return
+}
--- arrays/soma_test.go
+++ arrays/soma_test.go
@@ -1,6 +1,9 @@
 package arrays
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestArrays(t *testing.T) {
 
@@ -15,4 +18,37 @@ func TestArrays(t *testing.T) {
 		}
 	})
 
+}
+
+func TestSomaTudo(t *testing.T) {
+	resultado := SomaTudo([]int{1, 2}, []int{0, 9})
+	esperado := []int{3, 9}
+
+	if !reflect.DeepEqual(resultado, esperado) {
+		t.Errorf("resultado %v, esperado %v", resultado, esperado)
+	}
+}
+
+func TestSomaTodoOResto(t *testing.T) {
+
+	verificarSomas := func(t *testing.T, resultado, esperado []int) {
+		t.Helper()
+		if !reflect.DeepEqual(resultado, esperado) {
+			t.Errorf("resultado %v, esperado %v", resultado, esperado)
+		}
+	}
+
+	t.Run("Soma tudo menos o primeiro elemento", func(t *testing.T) {
+		resultado := SomaTodoOResto([]int{1, 2}, []int{0, 9})
+		esperado := []int{2, 9}
+
+		verificarSomas(t, resultado, esperado)
+	})
+
+	t.Run("Soma passando um array vazio", func(t *testing.T) {
+		resultado := SomaTodoOResto([]int{}, []int{0, 9, 10})
+		esperado := []int{0, 19}
+
+		verificarSomas(t, resultado, esperado)
+	})
 }
