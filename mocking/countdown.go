609c7c69fc9155c3793834143b3af35b54216e97
Renato <renatojlelis@gmail.com>
2020-10-11 23:37:44 -0300

    Begin mocking

---
+++ mocking/countdown.go
@@ -0,0 +1,13 @@
+package mocking
+
+import (
+	"fmt"
+	"io"
+)
+
+func Countdown(out io.Writer) {
+	for i := 3; i > 0; i-- {
+		fmt.Fprintln(out, i)
+	}
+	fmt.Fprintf(out, "Go!")
+}
---
+++ mocking/countdown_test.go
@@ -0,0 +1,22 @@
+package mocking
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestCountDown(t *testing.T) {
+	buffer := &bytes.Buffer{}
+
+	Countdown(buffer)
+
+	got := buffer.String()
+	want := `3
+2
+1
+Go!`
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
